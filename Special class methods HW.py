e5aeaf668a1aee680bafefbeefccd167564018ba
Maxim <domnin.94@mail.ru>
2024-05-04 12:33:34 +0300

    Специальные методы классов ДЗ

--- Special class methods HW.py
+++ Special class methods HW.py
@@ -3,8 +3,9 @@ class House:
     def __init__(self):
         self.numberOfFloors = 0
     def setNewNumberOfFloors(self, floors):
-        if floors >= 0:
-            print('numberOfFloors')
+        if floors > 0:
+            self.numberOfFloors = floors
+            print('numberOfFloors:', self.numberOfFloors)
 
 house = House()
-house.setNewNumberOfFloors(0)
+house.setNewNumberOfFloors(6)
