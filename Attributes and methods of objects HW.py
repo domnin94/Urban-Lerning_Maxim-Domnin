27e07caeab1cc35d3e38d38d933b20ea88c4adc5
Maxim <domnin.94@mail.ru>
2024-05-04 10:15:10 +0300

    Атрибуты и методы объекта

--- Attributes and methods of objects HW.py
+++ Attributes and methods of objects HW.py
@@ -2,13 +2,6 @@ class House:
     def __init__(self):
         self.numberOfFloors = 10
 
-    def floor(self):
-        print('Спускаемся')
-        self.current_floor = 8
-
 my_home = House()
-
-print('Всего этажей', my_home.numberOfFloors)
-my_home.floor()
-print('Текущий этаж', my_home.current_floor)
-
+for i in range(my_home.numberOfFloors):
+    print('Текущий этаж', my_home.numberOfFloors)
