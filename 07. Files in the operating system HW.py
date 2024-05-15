4a13b83aed8204ec74aa370b18c29baaa021f293
Maxim <domnin.94@mail.ru>
2024-05-15 12:38:07 +0300

    Файлы лперационной системы

---
+++ 07. Files in the operating system HW.py
@@ -0,0 +1,14 @@
+import time
+import os
+
+directory = 'C:\\Users\\Marina\\Maxim'
+
+for dirpath, dirnames, filenames in os.walk(directory):
+    for file in filenames:
+        filepath = os.path.join(dirpath, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт,' 
+            f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
