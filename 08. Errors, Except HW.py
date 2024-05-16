24d47da76877199da5b8cefa89f5c6167d54c1e6
Maxim <domnin.94@mail.ru>
2024-05-16 19:23:52 +0300

    Ошибки и исключения 2

--- 08. Errors, Except HW.py
+++ 08. Errors, Except HW.py
@@ -4,6 +4,6 @@ def add_everything_up(a, b):
     except TypeError as exc:
         print('Складывать нельзя, потому-что', exc, 'но мы все равно сложим')
     finally:
-        print('Cложили', a, b)
+        print('Cложили', str(a) + b)
 
 print(add_everything_up(120, 'cars'))
