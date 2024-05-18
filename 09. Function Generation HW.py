1d280eff5261719495d6d8762229896345eac93c
Maxim <domnin.94@mail.ru>
2024-05-18 16:46:05 +0300

    Генерация функций

---
+++ 09. Function Generation HW.py
@@ -0,0 +1,41 @@
+def create_operation(operation):
+    if operation == "multiplier":
+        def multiplier(x, y):
+            return x * y
+
+        return multiplier
+    elif operation == "division":
+        def division(x, y):
+            return x / y
+
+        return division
+
+
+my_func_add = create_operation("multiplier")
+print(my_func_add(1, 2))
+my_func_add = create_operation("division")
+print(my_func_add(1, 2))
+
+
+home = lambda x, y: x-y
+print(home(10, 3))
+
+
+def home_1(a, b):
+    return a - b
+
+print(home_1(20, 12))
+
+
+class Home:
+    def __init__(self, a):
+        self.a = a
+
+
+    def __call__(self, b):
+        return self.a * b
+
+
+result = Home(a=5)
+result1 = result(b=5)
+print(f'Площадь: {result1}')
