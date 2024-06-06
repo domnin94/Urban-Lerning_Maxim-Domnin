7c78cfddb0262e6cb4ab1a10fa854e028baed898
Maxim <domnin.94@mail.ru>
2024-06-06 20:19:14 +0300

    Потоки на классах

---
+++ 10.2. Threads on classes HW.py
@@ -0,0 +1,39 @@
+import time
+from collections import defaultdict
+from threading import Thread
+
+class Knights(Thread):
+
+    def __init__(self, name, skill, *args, **kwargs):
+        super(Knights, self).__init__(*args, **kwargs)
+        self.name = name
+        self.enemies = 100
+        self.skill = skill
+        self.day = 0
+
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        self.day += 1
+        self.enemies -= self.skill
+        print('{}, сражается {} день(дня), врагов осталось {}'.format(
+                self.name, self.day, self.enemies))
+        while self.enemies > 0:
+            self.day += 1
+            self.enemies -= self.skill
+            print('{}, сражается {} день(дня), врагов осталось {}'.format(
+                self.name, self.day, self.enemies))
+            if self.enemies == 0:
+                print('{} одержал победу, спустя {} дней!'.format(self.name, self.day))
+
+
+
+
+sir_lancelot = Knights(name='Sir Lancelot', skill=10)
+sir_galahad = Knights(name='Sir Galahad', skill=20)
+
+sir_lancelot.start()
+sir_galahad.start()
+
+sir_lancelot.join()
+sir_galahad.join()
